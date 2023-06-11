527dbc96c126502c6c29838081dff3f52def03e2
kamiltarach <113509888+kamiltarach@users.noreply.github.com>
2023-06-11 22:27:02 +0200

    usuniecie komentarzy

--- mes.php
+++ mes.php
@@ -4,24 +4,17 @@ $user = 'root';
 $password = '';
 $database = 'mes';
 
-// Sprawdzanie, czy przesłano formularz
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Pobieranie wartości z parametrów POST
   $recipient = $_POST['recipient'];
   $message = $_POST['message'];
 
-  // Nawiązywanie połączenia z bazą danych
   $conn = new mysqli($host, $user, $password, $database);
 
-  // Sprawdzanie błędów połączenia
   if ($conn->connect_error) {
     die("Błąd połączenia z bazą danych: " . $conn->connect_error);
   }
-
-  // Tworzenie zapytania SQL
   $sql = "INSERT INTO messages (rozmowca, wiadomosc) VALUES ('$recipient', '$message')";
 
-  // Wykonanie zapytania
   if ($conn->query($sql) === TRUE) {
     echo "Wiadomość została zapisana w bazie danych";
   } else {
